63366d5033c393b02dccd6171d0f1c1d60ff186a
Sechan Kim <rlatpcks77@gmail.com>
2021-08-12 18:02:14 +0900

    마무리 작업

--- js/todo.js
+++ js/todo.js
@@ -10,6 +10,8 @@ function saveTodos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(todosArray))
 }
 
+
+
 function replaceTodo(event) {
   event.preventDefault()
   const li = event.target.parentElement
@@ -18,10 +20,11 @@ function replaceTodo(event) {
   button[1].classList.add("hidden")
   const targetNode = event.target.parentElement
   const targetNodeId = targetNode.id
-  console.log(targetNode)
 
+  const replaceFormContainer = document.createElement("div")
+  li.appendChild(replaceFormContainer)
   const replaceForm = document.createElement("form")
-  li.appendChild(replaceForm)
+  replaceFormContainer.appendChild(replaceForm)
   replaceForm.classList.add("replace-form")
   let replaceInput = document.createElement("input")
   replaceForm.appendChild(replaceInput)
@@ -34,36 +37,48 @@ function replaceTodo(event) {
   replaceForm.appendChild(replaceInputSubmit)
   replaceInputSubmit.classList.add("replace-btn")
 
-  const replaceInputInit = document.createElement("input")
-  replaceInputInit.type = "submit"
-  replaceInputInit.value = "☓"
-  replaceForm.appendChild(replaceInputInit)
-  replaceInputInit.classList.add("replace-init-btn")
-  replaceInputInit.addEventListener("submit", (event) => {
-    button[0].classList.remove("hidden");
-    button[1].classList.remove("hidden");
-    event.preventDefault(); // 이거 안댐
-  })
-  // 수정하다가 X버튼 누르면 수정칸이 사라져야함
-
-  const span = targetNode.querySelector("span")
+  let span = targetNode.querySelector("span")
   replaceInput.setAttribute('placeholder', span.innerHTML)
   replaceInput.setAttribute('maxlength', 20)
   span.classList.add("hidden")
 
+  const replaceInputInit = document.createElement("input")
+  replaceInputInit.type = "button"
+  replaceInputInit.value = "☓"
+  replaceForm.appendChild(replaceInputInit)
+  replaceInputInit.classList.add("replace-init-btn")
+
+  replaceInputInit.addEventListener("click", replaceUndo)
+
 
   replaceForm.addEventListener("submit", (event) => {
-    event.preventDefault()
-    span.classList.remove("hidden")
-    const replaceInputValue = replaceInput.value
-    replaceForm.innerHTML = ""
-    span.innerHTML = replaceInputValue
-    const editTodo = todosArray.filter(function (todosArray) { return todosArray.id == targetNodeId });
-    editTodo[0].text = replaceInputValue
-    button[0].classList.remove("hidden")
-    button[1].classList.remove("hidden")
-    saveTodos()
+  event.preventDefault()
+  span.classList.remove("hidden")
+  const replaceInputValue = replaceInput.value
+  replaceForm.innerHTML = ""
+  span.innerHTML = replaceInputValue
+  const editTodo = todosArray.filter(function (todosArray) { return todosArray.id == targetNodeId });
+  editTodo[0].text = replaceInputValue
+  button[0].classList.remove("hidden")
+  button[1].classList.remove("hidden")
+  saveTodos()
   })
+
+
+  function replaceUndo (event) {
+    event.preventDefault();
+    span.classList.remove("hidden")
+    button[0].classList.remove("hidden");
+    button[1].classList.remove("hidden");
+    replaceFormContainer.classList.add('hidden')
+  }
+
+  document.addEventListener("mouseup", (event) => {
+    if (event.target !== replaceInput) {
+      replaceUndo(event)
+     } 
+  })
+
 }
 
 
